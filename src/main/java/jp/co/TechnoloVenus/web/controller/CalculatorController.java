cf693c708959d9543f04709ae710f04a279f4c98
funayu <funayu0908@gmail.com>
2019-08-31 20:16:06 +0900

    クラスコメントの追加、メソッドコメントの追加、cssファイルから不要な記述を削除

--- src/main/java/jp/co/TechnoloVenus/web/controller/CalculatorController.java
+++ src/main/java/jp/co/TechnoloVenus/web/controller/CalculatorController.java
@@ -3,6 +3,9 @@ package jp.co.TechnoloVenus.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * calculatorページのURLを定義するクラス
+ */
 @Controller
 public class CalculatorController {
 	@GetMapping("/calculator")
--- src/main/java/jp/co/TechnoloVenus/web/controller/YozoraController.java
+++ src/main/java/jp/co/TechnoloVenus/web/controller/YozoraController.java
@@ -3,8 +3,15 @@ package jp.co.TechnoloVenus.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * yozoraページのURLを定義するクラス
+ */
 @Controller
 public class YozoraController {
+	/**
+	 * yozoraページのhtmlファイル名を返すメソッド
+	 * @return htmlファイル名
+	 */
 	@GetMapping("/yozora")
 	public String goYozora() {
 		return "yozora";
