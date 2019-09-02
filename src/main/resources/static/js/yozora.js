38d6cf5aa3213e6adf210403a248cc6e4d4c92ba
funayu <funayu0908@gmail.com>
2019-09-02 23:04:39 +0900

    コメント追加、varをconstに修正、漢字変換ミス修正、数式の表現を修正

--- src/main/resources/static/js/yozora.js
+++ src/main/resources/static/js/yozora.js
@@ -1,7 +1,8 @@
+// phina.jsの全てのクラスをグローバルに展開
 phina.globalize();
 
 // アセットの定義
-var ASSETS = {
+const ASSETS = {
   image: {
     // 魔女っ子の画像
     'majokko': 'https://raw.githubusercontent.com/funayu/storage/images/008.png',
@@ -14,7 +15,7 @@ var ASSETS = {
  * メインシーンの処理
  */
 phina.define('MainScene', {
-  // 繼承
+  // 継承
   superClass: 'CanvasScene',
   // コンストラクタ
   init: function() {
@@ -25,10 +26,10 @@ phina.define('MainScene', {
     Sprite('bg').addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
 
     //魔女っ子画像作成
-  　var majokko = Sprite('majokko').addChildTo(this);
+  　const majokko = Sprite('majokko').addChildTo(this);
   　
     // 初期位置
-  　majokko.x = (this.gridX.center())-200;
+  　majokko.x = this.gridX.center() - 200;
   　majokko.y = this.gridY.center();
     // 画像サイズ
   　majokko.width = 150;
@@ -41,9 +42,9 @@ phina.define('MainScene', {
       // 画像がフレームの右端、または左端まで行ったかを判定する
       if(this.right === 600 || this.left===0){
         // 移動方向を反転する
-        this.vx *=-1;
+        this.vx = -(this.vx);
         // 魔女っ子画像の向きを反転する
-        this.scaleX *=-1;
+        this.scaleX = -(this.scaleX);
       } 
       
       // 魔女っ子の移動
@@ -57,11 +58,11 @@ phina.define('MainScene', {
 */
 phina.main(function() {
   // アプリケーションを生成
-  var app = GameApp({
-    // メインシーンから開始
-    startLabel: 'main',
-    // アセット読み込み
-    assets: ASSETS,
+  const app = GameApp({
+  // メインシーンから開始
+  startLabel: 'main',
+  // アセット読み込み
+  assets: ASSETS,
   });
   
   // アプリケーションの実行
