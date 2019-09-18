4f174152501466f534f1fcf2ae5613f26268c031
funayu <funayu0908@gmail.com>
2019-09-18 23:07:59 +0900

    スペルミス修正

--- src/main/java/jp/co/TechnoloVenus/web/controller/ColorfulBallController.java
+++ src/main/java/jp/co/TechnoloVenus/web/controller/ColorfulBallController.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * colorfull_ballページのURLを定義するクラス
+ * colorful_ballページのURLを定義するクラス
  */
 @Controller
 public class ColorfulBallController {
 	/**
-	 * colorfull_ballページのhtmlファイル名を返すメソッド
+	 * colorful_ballページのhtmlファイル名を返すメソッド
 	 * @return htmlファイル名
 	 */
-	@GetMapping("/colorfull_ball")
-	public String gocolorfull_ball() {
-		return "colorfull_ball";
+	@GetMapping("/colorful_ball")
+	public String goColorful_ball() {
+		return "colorful_ball";
 	}
 }
