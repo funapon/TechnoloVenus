a4b4f10bbe08fd7a5ca46ddca6ecf4b2b848ffab
funayu <funayu0908@gmail.com>
2019-09-21 17:14:49 +0900

    ボールの情報をクラスで定義するように修正

--- src/main/resources/static/js/processing/colorful_ball.js
+++ src/main/resources/static/js/processing/colorful_ball.js
@@ -1,49 +1,67 @@
 // キャンバス上のボールの数
 const NUM_BALL = 65;
-
-// ボールの半径
-const ballRadius = new Array(NUM_BALL);
-// ボールの半径最小値
-const ballRadiusMin = 25;
-// ボールの半径最大値
-const ballRadiusMax = 50;
-
-// ボールの中心座標
-const ballCenterX = new Array(NUM_BALL);
-const ballCenterY = new Array(NUM_BALL);
-
-// ボールの速度
-const ballVelocityX = new Array(NUM_BALL);
-const ballVelocityY = new Array(NUM_BALL);
-// ボールの速度最小値
-const ballVelocityMin = -2.5;
-// ボールの速度最大値
-const ballVelocityMax = 2.5;
-
-// ボールの色
-const ballColor = new Array(NUM_BALL);
-// ボールの透明度、0～255で設定
-const ballAlpha = 85;
-
 // キャンバスの背景色
 const BACKGROUND_COLOR = 255;
+const balls = new Array(NUM_BALL);
+
+class Ball {
+  // 半径最小値
+  RADIUS_MIN;
+  // 半径最大値
+  RADIUS_MAX;
+  // 移動速度最小値
+  VELOCITY_MIN;
+  // 移動速度最大値
+  VELOCITY_MAX;
+  // 透明度、0～255で設定
+  ALPHA;
+  // 半径
+  radius;
+  // 中心座標
+  centerX;
+  centerY;
+  //速度
+  velocityX;
+  velocityY;
+  // 色
+  color;
+
+  constructor(width, height) {
+    this.RADIUS_MAX = 50;
+    this.RADIUS_MIN = 25;
+    this.VELOCITY_MIN = -2.5;
+    this.VELOCITY_MAX = 2.5;
+    this.ALPHA = 85;
+
+    this.radius = 
+      Math.floor(Math.random() * (this.RADIUS_MAX + 1 - this.RADIUS_MIN)) + this.RADIUS_MIN;
+    // ボールがキャンバスの壁に埋まった状態でスタートしないように中心座標を設定、widthはキャンバスの幅、heightはキャンバスの高さ
+    this.centerX = random(this.radius, width - this.radius);
+    this.centerY = random(this.radius, height - this.radius);
+    this.velocityX = random(this.VELOCITY_MIN, this.VELOCITY_MAX);
+    this.velocityY = random(this.VELOCITY_MIN, this.VELOCITY_MAX);
+    this.color = color(random(255), random(255), random(255), this.ALPHA);
+  }
+
+  // ボールの位置を更新
+  updatePosition() {
+    this.centerX += this.velocityX;
+    this.centerY += this.velocityY;
+  }
+}
 
 // 初期化処理
 // 一度だけ実行される
 function setup() {
   // キャンバスの作成
   createCanvas(960, 540);
-  
   // ボールの初期値を設定
   for (let i=0; i<NUM_BALL; i++){
-    ballRadius[i] = randomIntMinMax(ballRadiusMin, ballRadiusMax);
-    // ボールがキャンバスの壁に埋まった状態でスタートしないように中心座標を設定、widthはキャンバスの幅、heightはキャンバスの高さ
-    ballCenterX[i] = random(ballRadius[i], width - ballRadius[i]);
-    ballCenterY[i] = random(ballRadius[i], height - ballRadius[i]);
-    ballVelocityX[i] = random(ballVelocityMin, ballVelocityMax);
-    ballVelocityY[i] = random(ballVelocityMin, ballVelocityMax);
-    ballColor[i] = color(random(255), random(255), random(255), ballAlpha);
+    balls[i] = new Ball(width, height);
   }
+
+  // ボールの枠線は無しにする
+  noStroke();
 }
 
 // メインループ処理
@@ -53,31 +71,24 @@ function draw() {
   background(BACKGROUND_COLOR);
 
   for(let i=0; i<NUM_BALL; i++) {
+    const ball = balls[i];
+
     // ボールの位置を更新
-    ballCenterX[i] += ballVelocityX[i];
-    ballCenterY[i] += ballVelocityY[i];
+    ball.updatePosition();
 
     // 左右の壁との当たり判定
-    if(ballCenterX[i] - ballRadius[i] <= 0 || ballCenterX[i] + ballRadius[i] >= width) {
-      ballVelocityX[i] *= -1;
+    if(ball.centerX - ball.radius <= 0 || ball.centerX + ball.radius >= width) {
+      ball.velocityX *= -1;
     }
 
     // 上下の壁との当たり判定
-    if(ballCenterY[i] - ballRadius[i] <= 0 || ballCenterY[i] + ballRadius[i] >= height) {
-      ballVelocityY[i] *= -1;
+    if(ball.centerY - ball.radius <= 0 || ball.centerY + ball.radius >= height) {
+      ball.velocityY *= -1;
     }
     
-    // ボールの枠線は無しにする
-    noStroke();
     // ボールの塗りつぶし色を設定する
-    fill(ballColor[i]);
+    fill(ball.color);
     // ボールを描画する
-    ellipse(ballCenterX[i], ballCenterY[i], 2 * ballRadius[i], 2 * ballRadius[i]);
+    ellipse(ball.centerX, ball.centerY, 2 * ball.radius, 2 * ball.radius);
   }
-}
-
-// minからmaxの範囲で乱数を発生させる関数
-function randomIntMinMax(min, max) {
-  const rdm = Math.floor(Math.random() * (max + 1 - min)) + min;
-  return rdm;
 }
