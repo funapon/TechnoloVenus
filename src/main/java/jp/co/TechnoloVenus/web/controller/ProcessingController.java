e895a41e049e8e0c9ecc2f96be8f9ac68697c93c
funayu <funayu0908@gmail.com>
2019-09-21 15:13:52 +0900

    processing関連はフォルダにまとめた、演算子の前後にスペースを入れた

--- src/main/java/jp/co/TechnoloVenus/web/controller/ProcessingController.java
+++ src/main/java/jp/co/TechnoloVenus/web/controller/ProcessingController.java
@@ -2,18 +2,28 @@ package jp.co.TechnoloVenus.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * colorful_ballページのURLを定義するクラス
  */
 @Controller
-public class ColorfulBallController {
+@RequestMapping("/processing")
+public class ProcessingController {
+	/**
+	 * processingページのhtmlファイル名を返すメソッド
+	 * @return htmlファイル名
+	 */	
+	@GetMapping
+	public String goProcessing() {
+		return "processing";
+	}
 	/**
 	 * colorful_ballページのhtmlファイル名を返すメソッド
 	 * @return htmlファイル名
 	 */
 	@GetMapping("/colorful_ball")
 	public String goColorful_ball() {
-		return "colorful_ball";
-	}
+		return "processing/colorful_ball";
+	}   
 }
--- src/main/resources/static/js/processing/colorful_ball.js
+++ src/main/resources/static/js/processing/colorful_ball.js
@@ -1,32 +1,32 @@
 // キャンバス上のボールの数
-const numBalls = 65;
+const NUM_BALL = 65;
 
 // ボールの半径
-const ballRadius = new Array(numBalls);
+const ballRadius = new Array(NUM_BALL);
 // ボールの半径最小値
 const ballRadiusMin = 25;
 // ボールの半径最大値
 const ballRadiusMax = 50;
 
 // ボールの中心座標
-const ballCenterX = new Array(numBalls);
-const ballCenterY = new Array(numBalls);
+const ballCenterX = new Array(NUM_BALL);
+const ballCenterY = new Array(NUM_BALL);
 
 // ボールの速度
-const ballVelocityX = new Array(numBalls);
-const ballVelocityY = new Array(numBalls);
+const ballVelocityX = new Array(NUM_BALL);
+const ballVelocityY = new Array(NUM_BALL);
 // ボールの速度最小値
 const ballVelocityMin = -2.5;
 // ボールの速度最大値
 const ballVelocityMax = 2.5;
 
 // ボールの色
-const ballColor = new Array(numBalls);
+const ballColor = new Array(NUM_BALL);
 // ボールの透明度、0～255で設定
 const ballAlpha = 85;
 
 // キャンバスの背景色
-const backGroundColor = 255;
+const BACKGROUND_COLOR = 255;
 
 // 初期化処理
 // 一度だけ実行される
@@ -35,11 +35,11 @@ function setup() {
   createCanvas(960, 540);
   
   // ボールの初期値を設定
-  for (let i=0; i<numBalls; i++){
+  for (let i=0; i<NUM_BALL; i++){
     ballRadius[i] = randomIntMinMax(ballRadiusMin, ballRadiusMax);
     // ボールがキャンバスの壁に埋まった状態でスタートしないように中心座標を設定、widthはキャンバスの幅、heightはキャンバスの高さ
-    ballCenterX[i] = random(ballRadius[i], width-ballRadius[i]);
-    ballCenterY[i] = random(ballRadius[i], height-ballRadius[i]);
+    ballCenterX[i] = random(ballRadius[i], width - ballRadius[i]);
+    ballCenterY[i] = random(ballRadius[i], height - ballRadius[i]);
     ballVelocityX[i] = random(ballVelocityMin, ballVelocityMax);
     ballVelocityY[i] = random(ballVelocityMin, ballVelocityMax);
     ballColor[i] = color(random(255), random(255), random(255), ballAlpha);
@@ -50,20 +50,20 @@ function setup() {
 // setupの実行後に繰り返し実行される
 function draw() {
   // キャンバスの背景色を設定
-  background(backGroundColor);
+  background(BACKGROUND_COLOR);
 
-  for(let i=0; i<numBalls; i++) {
+  for(let i=0; i<NUM_BALL; i++) {
     // ボールの位置を更新
     ballCenterX[i] += ballVelocityX[i];
     ballCenterY[i] += ballVelocityY[i];
 
     // 左右の壁との当たり判定
-    if(ballCenterX[i]-ballRadius[i] <= 0 || ballCenterX[i]+ballRadius[i] >= width) {
+    if(ballCenterX[i] - ballRadius[i] <= 0 || ballCenterX[i] + ballRadius[i] >= width) {
       ballVelocityX[i] *= -1;
     }
 
     // 上下の壁との当たり判定
-    if(ballCenterY[i]-ballRadius[i] <= 0 || ballCenterY[i]+ballRadius[i] >= height) {
+    if(ballCenterY[i] - ballRadius[i] <= 0 || ballCenterY[i] + ballRadius[i] >= height) {
       ballVelocityY[i] *= -1;
     }
     
@@ -72,7 +72,7 @@ function draw() {
     // ボールの塗りつぶし色を設定する
     fill(ballColor[i]);
     // ボールを描画する
-    ellipse(ballCenterX[i], ballCenterY[i], 2*ballRadius[i], 2*ballRadius[i]);
+    ellipse(ballCenterX[i], ballCenterY[i], 2 * ballRadius[i], 2 * ballRadius[i]);
   }
 }
 
